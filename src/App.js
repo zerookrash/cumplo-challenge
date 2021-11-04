ca9d8d7d44ec14c936c8705d384913079a9b0352
MarioVillelaLarraza <mario.v@oblekco.com>
2021-11-04 11:43:39 -0600

    Inicio de proyecto, instalaciones basicas (axios y bootstrap), adaptacion del layout de la app.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+import logo from "./assets/img/logo.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <img src={logo} alt="logo" className="img-fluid m-2" />
+
+        <h3>30:00</h3>
       </header>
+      <div>
+        <h1>El empleado del mes</h1>
+      </div>
     </div>
   );
 }
